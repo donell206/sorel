3b9e5631e994bd61244da265bc58829b97bda355
Donnel <donnel206@hotmail.com>
2021-11-09 13:56:14 +0100

    API Paging, Filtering, Sorting & Searching

--- API/Startup.cs
+++ API/Startup.cs
@@ -40,6 +40,17 @@ namespace API
 
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+                        services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
+
+
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,7 +66,7 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles();
-
+            app.UseCors("CorsPolicy");
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
